2a4d49cfc80dbb04e8f7bf7482c396de216fddee
Júnior de Oliveira <ellismar.oliveira@syngenta.com>
2023-05-18 14:10:15 -0300

    refactor: using postgres database

--- src/Checkout.ts
+++ src/Checkout.ts
@@ -1,8 +1,9 @@
-import { products, coupons } from "./api-rest";
+import pgPromise from "pg-promise";
 import { validateCpf } from "./validator";
 
 export class Checkout {
   async execute(input: Input): Promise<Output> {
+    const connection = pgPromise()("postgres://postgres:123456@localhost:5432/app")
     const output = {
       subtotal: 0,
       total: 0,
@@ -15,17 +16,17 @@ export class Checkout {
           for (const item of input.items) {
             if (item.quantity <= 0) throw new Error('Invalid quantity')
             if (input.items.filter((i: any) => i.idProduct === item.idProduct).length > 1) throw new Error('Duplicated item');
-            const productData = products.filter(product => product.id === item.idProduct);
-            if (productData[0].width <= 0 ||
-              productData[0].height <= 0 ||
-              productData[0].length <= 0) {
+            const [productData] = await connection.query('SELECT * FROM cccat11.product WHERE id_product = $1', item.idProduct);
+            if (productData.width <= 0 ||
+              productData.height <= 0 ||
+              productData.length <= 0) {
               throw new Error('Invalid dimensions');
             }
-            if (productData[0].weight <= 0) throw new Error('Invalid weight');
-            output.subtotal += productData[0].price * item.quantity;
+            if (productData.weight <= 0) throw new Error('Invalid weight');
+            output.subtotal += productData.price * item.quantity;
             if (input.from && input.to) {
-              const volume = productData[0].width / 100 * productData[0].height / 100 * productData[0].length / 100;
-              const density = productData[0].weight / volume;
+              const volume = productData.width / 100 * productData.height / 100 * productData.length / 100;
+              const density = productData.weight / volume;
               let freight = volume * 1000 * (density / 100);
               freight = Math.max(freight, 10);
               output.freight += freight * item.quantity;
@@ -34,7 +35,7 @@ export class Checkout {
         }
         output.total = output.subtotal;
         if (input.coupon) {
-          const couponData = coupons.find(coupon => coupon.code === input.coupon);
+          const [couponData] = await connection.query('SELECT * FROM cccat11.coupon WHERE code = $1', input.coupon);
           const today = new Date();
           if (couponData && couponData.expire_date.getTime() >= today.getTime()) {
             output.total -= output.total * (couponData.percentage / 100);
--- src/api-rest.ts
+++ src/api-rest.ts
@@ -17,19 +17,6 @@ interface Coupon {
   expire_date: Date;
 }
 
-export const products: Product[] = [
-  { id: 1, description: 'A', price: 1000, width: 100, height: 30, length: 10, weight: 3 },
-  { id: 2, description: 'B', price: 5000, width: 50, height: 50, length: 50, weight: 22 },
-  { id: 3, description: 'C', price: 30, width: 10, height: 10, length: 10, weight: 0.9 },
-  { id: 4, description: 'D', price: 30, width: -10, height: -10, length: -10, weight: -1 },
-  { id: 5, description: 'D', price: 30, width: 10, height: 10, length: 10, weight: -1 },
-];
-
-export const coupons: Coupon[] = [
-  { percentage: 20, code: 'VALE20', expire_date: new Date('2023-10-01T10:00:00') },
-  { percentage: 10, code: 'VALE10', expire_date: new Date('2022-10-01T10:00:00') },
-];
-
 const app = express();
 
 app.use(express.json());
